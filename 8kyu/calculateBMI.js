1acf25dc5746e8318d4b08b984c928c3f44bfda0
Altin Gruda <altingruda@gmail.com>
2022-07-08 11:03:58 +0200

    Update calculateBMI

--- 8kyu/calculateBMI.js
+++ 8kyu/calculateBMI.js
@@ -10,5 +10,5 @@
 
 function bmi(weight, height) {
     var bmi = weight / Math.pow(height, 2);
-    return weight / height <= 18.5 ? 'Underweight' : ;
+    return bmi <= 18.5 ? 'Underweight' : bmi<= 25.0 ? 'Normal' : bmi<= 30.0 ? 'Overweight' : 'Obese';
   }
