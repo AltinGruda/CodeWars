6c87aa041bb09a0895fd5a1e15149f5f676da6af
Altin Gruda <altingruda@gmail.com>
2022-09-18 12:56:48 +0200

    create PaginationHelper

---
+++ 5kyu/PaginationHelper.js
@@ -0,0 +1,29 @@
+function PaginationHelper(collection, itemsPerPage){
+    this.collection = collection, this.itemsPerPage = itemsPerPage;
+  }
+  
+  // returns the number of items within the entire collection
+  PaginationHelper.prototype.itemCount = function() {
+    return this.collection.length;
+  }
+  
+  // returns the number of pages
+  PaginationHelper.prototype.pageCount = function() {
+    return Math.ceil(this.collection.length / this.itemsPerPage);
+  }
+
+  // returns the number of items on the current page. page_index is zero based.
+// this method should return -1 for pageIndex values that are out of range
+  PaginationHelper.prototype.pageItemCount = function(pageIndex) {
+    return pageIndex < this.pageCount() 
+      ? Math.min(this.itemsPerPage, this.collection.length - pageIndex * this.itemsPerPage)
+      : -1;
+  }
+  
+  // determines what page an item is on. Zero based indexes
+// this method should return -1 for itemIndex values that are out of range
+  PaginationHelper.prototype.pageIndex = function(itemIndex) {
+    return itemIndex < this.collection.length && itemIndex >= 0
+      ? Math.floor(itemIndex / this.itemsPerPage)
+      : -1;
+  }
