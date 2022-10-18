3148d7ba20d7dfecc4980f5fc3a62ce944586211
Altin Gruda <67205874+AltinGruda@users.noreply.github.com>
2022-10-18 09:55:37 +0200

    create twoToOne

---
+++ 7kyu/twoToOne.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+// A JavaScript Set is a collection of unique values. Each value can only occur once in a Set. A Set can hold any value of any data type.
+function longest(s1, s2) {
+    return Array.from(new Set(s1 + s2)).sort().join('');
+}
