25be8ef9d7ee003d45ad948c566cd37913273781
Altin Gruda <67205874+AltinGruda@users.noreply.github.com>
2022-10-30 11:52:16 +0100

    rentalCarCost optimization

--- 8kyu/rockPaperScissors.js
+++ 8kyu/rockPaperScissors.js
@@ -1,12 +1,12 @@
 //create a game to play rock paper scissors
 
 const rps = (p1, p2) => {
-    if (p1 === p2) return "Draw!";
-    var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
-    if (p2 === rules[p1]) {
-      return "Player 1 won!";
-    }
-    else {
-      return "Player 2 won!";
-    }
+  if (p1 === p2) return "Draw!";
+  let rules = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper"
   };
+
+  return p2 === rules[p1] ? "Player 1 won!" : "Player 2 won!"
+};
