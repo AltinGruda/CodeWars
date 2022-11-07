1a338c250f632486bd75d16abe76aaa7296b7ffd
Altin Gruda <67205874+AltinGruda@users.noreply.github.com>
2022-11-07 22:33:28 +0100

    optimized alphabetPosition

--- 6kyu/alphabetPosition.js
+++ 6kyu/alphabetPosition.js
@@ -13,11 +13,11 @@
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
 
 function alphabetPosition(text) {
-    var result = "";
-    for (var i = 0; i < text.length; i++){
-      var code = text.toUpperCase().charCodeAt(i)
-      if (code > 64 && code < 91) result += (code - 64) + " ";
-    }
-  
-    return result.slice(0, result.length - 1);
+  let result = "";
+  for (let i = 0; i < text.length; i++) {
+    let code = text.toUpperCase().charCodeAt(i)
+    if (code > 64 && code < 91) result += (code - 64) + " ";
   }
+
+  return result.slice(0, result.length - 1);
+}
