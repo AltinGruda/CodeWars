65fb65c550d7a851fcdd90a314cedadce9a0f1e9
AltinGruda <altingruda@gmail.com>
2022-11-22 17:31:05 +0100

    update between

--- 8kyu/between.js
+++ 8kyu/between.js
@@ -8,10 +8,8 @@
 
 function between(a, b) {
   let arr = [];
-  if (a < b) {
-    for (let i = a, j = 0; i <= b; i++, j++) {
-      arr[j] = i;
-    }
+  for (let i = a; i <= b; i++) {
+    arr.push(i);
   }
   return arr;
 }
