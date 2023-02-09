a8ce5388d4ffd8a4c21e3a1bcdbfdcafc8578b82
AltinGruda <altingruda@gmail.com>
2023-02-09 11:51:37 +0100

    create angle

---
+++ 7kyu/angle.js
@@ -0,0 +1,5 @@
+// Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+//The formula for calculating the sum of interior angles is ( n − 2 ) × 180 ∘ where is the number of sides.
+function angle(n) {
+  return (n - 2) * 180;
+}
