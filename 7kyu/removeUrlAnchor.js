9ad21975fd7b4548b17fc0206b59e116b34d64dd
AltinGruda <altingruda@gmail.com>
2023-02-23 17:18:56 +0100

    create removeUrlAnchor

---
+++ 7kyu/removeUrlAnchor.js
@@ -0,0 +1,12 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+function removeUrlAnchor(url) {
+  return url.split("#")[0];
+}
+
+removeUrlAnchor("www.codewars.com#about"); //, 'www.codewars.com')
+removeUrlAnchor("www.codewars.com/katas/?page=1#about"); //, 'www.codewars.com/katas/?page=1')
+removeUrlAnchor("www.codewars.com/katas/"); //, 'www.codewars.com/katas/')
