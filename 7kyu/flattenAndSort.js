72f7034d8230001c745ec0c009c081afa78d4baf
AltinGruda <altingruda@gmail.com>
2024-04-12 11:20:47 +0200

    flat and sort

--- 7kyu/flattenAndSort.js
+++ 7kyu/flattenAndSort.js
@@ -5,10 +5,6 @@
 // Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
 
 function flattenAndSort(array) {
-    for(let i = 0; i < array.length; i++){
-      array[i].sort((a, b) => a - b);
-    }
-    
     return array.flat().sort((a, b) => a-b);
 }
 
