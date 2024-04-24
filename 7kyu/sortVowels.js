72ea7dace33d13e8ed722b1fc3fc1134fb481b45
AltinGruda <altingruda@gmail.com>
2024-04-24 08:25:49 +0200

    sort vowels

---
+++ 7kyu/sortVowels.js
@@ -0,0 +1,5 @@
+function sortVowels(s){
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    if (typeof s === "string") return [...s].map(letter => vowels.includes(letter) ? `|${letter}` : `${letter}|`).join('\n');
+    return '';
+}
